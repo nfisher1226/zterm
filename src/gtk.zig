b6f8c822433bef4421c909c2bab35dfb1f571a36
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2021-05-25 14:48:54 -0400

    Recreate interface in Glade Move triple button action widget for notebook into menu Move certain variables to global scope in gui.zig

--- src/gtk.zig
+++ src/gtk.zig
@@ -8,3 +8,9 @@ pub fn g_signal_connect(instance: c.gpointer, detailed_signal: [*c]const c.gchar
     const flags: *c.GConnectFlags = @ptrCast(*c.GConnectFlags, &zero);
     return c.g_signal_connect_data(instance, detailed_signal, c_handler, data, null, flags.*);
 }
+
+pub fn builder_get_widget(builder: *c.GtkBuilder, name: [*]const u8) [*]c.GtkWidget {
+    var gobject = @ptrCast([*c]c.GTypeInstance, c.gtk_builder_get_object(builder, name));
+    var gwidget = @ptrCast([*c]c.GtkWidget, c.g_type_check_instance_cast(gobject, c.gtk_widget_get_type()));
+    return gwidget;
+}
--- src/gui.zig
+++ src/gui.zig
@@ -26,50 +26,47 @@ pub const Tab = struct {
 };
 
 pub const RunData = struct {
-    window: *c.GtkWindow,
-    notebook: *c.GtkWidget,
     opts: *Opts,
-    tabs: std.HashMap(*Tab),
+    tabs: std.AutoHashMap(u64, *Tab),
 };
 
-pub fn activate(a: *c.GtkApplication, appdata: c.gpointer) void {
+var builder: *c.GtkBuilder = undefined;
+var notebook: [*]c.GtkWidget = undefined;
+var window: [*]c.GtkWidget = undefined;
+var options: Opts = undefined;
+
+pub fn activate(application: *c.GtkApplication, appdata: c.gpointer) void {
     // Cast the gpointer to a normal pointer and dereference it, giving us
     // our "opts" struct initialized earlier.
-    const options = @ptrCast(*Opts, @alignCast(8, appdata)).*;
-    const window = c.gtk_application_window_new(a);
+    options = @ptrCast(*Opts, @alignCast(8, appdata)).*;
+
+    builder = c.gtk_builder_new();
+    var ret = c.gtk_builder_add_from_file(builder, "./src/gui.glade", @intToPtr([*c][*c]c._GError, 0));
+    if (ret == 0) {
+        stderr.print("builder file fail\n", .{}) catch unreachable;
+        std.process.exit(1);
+    }
+    c.gtk_builder_set_application(builder, application);
+
+    window = gtk.builder_get_widget(builder, "window");
     const window_ptr = @ptrCast(*c.GtkWindow, window);
     c.gtk_window_set_title(window_ptr, options.title);
 
-    var notebook = c.gtk_notebook_new();
+    notebook = gtk.builder_get_widget(builder, "notebook");
     const notebook_ptr = @ptrCast(*c.GtkNotebook, notebook);
 
-    const addbutton = c.gtk_button_new_from_icon_name("tab-new", menu_size);
-    c.gtk_widget_set_tooltip_text(addbutton, "Open new tab");
-
-    const splitbutton = c.gtk_button_new_from_icon_name("list-add", menu_size);
-    c.gtk_widget_set_tooltip_text(splitbutton, "Split view");
-
-    const rotatebutton = c.gtk_button_new_from_icon_name("object-rotate-right", menu_size);
-    c.gtk_widget_set_tooltip_text(rotatebutton, "Change split orientation");
-
-    const ctrlbox = c.gtk_box_new(horizontal, 0);
-
-    const ctrlbuttons = [3]*c.GtkWidget{ addbutton, splitbutton, rotatebutton };
-    for (ctrlbuttons) |button| {
-        c.gtk_button_set_relief(@ptrCast(*c.GtkButton, button), relief_none);
-        c.gtk_widget_set_has_tooltip(button, 1);
-        c.gtk_widget_set_can_focus(button, 0);
-        c.gtk_box_pack_start(@ptrCast(*c.GtkBox, ctrlbox), button, 0, 1, 1);
-    }
-
-    c.gtk_widget_show_all(ctrlbox);
-    c.gtk_notebook_set_action_widget(notebook_ptr, ctrlbox, pack_end);
+    const new_tab = gtk.builder_get_widget(builder, "new_tab");
+    const split_view = gtk.builder_get_widget(builder, "split_view");
+    const rotate_view = gtk.builder_get_widget(builder, "rotate_view");
+    const preferences = gtk.builder_get_widget(builder, "preferences");
+    const close_tab = gtk.builder_get_widget(builder, "close_tab");
+    const quit_app = gtk.builder_get_widget(builder, "quit_app");
 
     _ = gtk.g_signal_connect(
-        addbutton,
-        "clicked",
-        @ptrCast(c.GCallback, addbutton_callback),
-        @ptrCast(c.gpointer, notebook),
+        new_tab,
+        "activate",
+        @ptrCast(c.GCallback, new_tab_callback),
+        null,
     );
 
     const command = @ptrCast([*c][*c]c.gchar, &([2][*c]c.gchar{
@@ -77,7 +74,7 @@ pub fn activate(a: *c.GtkApplication, appdata: c.gpointer) void {
         null,
     }));
 
-    const tab = new_tab(notebook_ptr, command);
+    const tab = new_tab_init(command);
     // We have to get the terminal in order to grab focus, use an
     // iterator and return the first (and only) entry's value field
     var term = if (tab.terms.iterator().next()) |entry| termblk: {
@@ -91,16 +88,15 @@ pub fn activate(a: *c.GtkApplication, appdata: c.gpointer) void {
         null,
     );
 
-    c.gtk_container_add(@ptrCast(*c.GtkContainer, window), notebook);
-
+    std.debug.print("Application ready\n", .{});
     c.gtk_widget_show_all(window);
     c.gtk_widget_grab_focus(@ptrCast(*c.GtkWidget, term));
+    c.gtk_main();
 }
 
-fn addbutton_callback(button: *c.GtkButton, notebook: c.gpointer) void {
+fn new_tab_callback(menuitem: *c.GtkMenuItem, user_data: c.gpointer) void {
     const command = @ptrCast([*c]const u8, os.getenvZ("SHELL") orelse "/bin/sh");
-    _ = new_tab(
-        @ptrCast(*c.GtkNotebook, @alignCast(8, notebook)),
+    _ = new_tab_init(
         @ptrCast([*c][*c]c.gchar, &([2][*c]c.gchar{
             c.g_strdup(command),
             null,
@@ -129,7 +125,7 @@ fn new_term(command: [*c][*c]c.gchar) *c.GtkWidget {
     return term;
 }
 
-fn new_tab(notebook: *c.GtkNotebook, command: [*c][*c]c.gchar) Tab {
+fn new_tab_init(command: [*c][*c]c.gchar) Tab {
     const term = new_term(command);
     _ = gtk.g_signal_connect(
         term,
@@ -171,8 +167,9 @@ fn new_tab(notebook: *c.GtkNotebook, command: [*c][*c]c.gchar) Tab {
     };
 
     c.gtk_widget_show(@ptrCast(*c.GtkWidget, term));
-    _ = c.gtk_notebook_append_page(notebook, term, 0);
-    c.gtk_notebook_set_tab_label(notebook, @ptrCast(*c.GtkWidget, term), @ptrCast(*c.GtkWidget, box));
+    const notebook_ptr = @ptrCast(*c.GtkNotebook, notebook);
+    _ = c.gtk_notebook_append_page(notebook_ptr, term, 0);
+    c.gtk_notebook_set_tab_label(notebook_ptr, @ptrCast(*c.GtkWidget, term), @ptrCast(*c.GtkWidget, box));
     return tab;
 }
 
