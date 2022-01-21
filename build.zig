d96d96bda346a7a89baf5a3f24a821223cf1c3c8
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-01-20 23:25:26 -0500

    build.zig - add `size` option

--- build.zig
+++ build.zig
@@ -22,11 +22,14 @@ pub fn build(b: *Builder) void {
         deps.pkgs.addAllTo(exe);
     }
 
+    // set the `datadir`
     const datadir = b.option(
         []const u8,
         "datadir",
         "Path to install data files (relative tp prefix)"
     ) orelse "share";
+
+    // get some paths
     const desktop_path = fs.path.join(
         b.allocator,
         &[_][]const u8{ datadir, "/applications/zterm.desktop" },
@@ -35,14 +38,43 @@ pub fn build(b: *Builder) void {
         b.allocator,
         &[_][]const u8{ datadir, "/icons/hicolor/scalable/apps/zterm.svg" },
     ) catch unreachable;
+    // install data files
     b.installFile("data/zterm.desktop", desktop_path);
     b.installFile("data/zterm.svg", icon_path);
 
+    // `strip` option
     const strip = b.option(bool, "strip", "Strip the installed executable") orelse false;
     if (strip) {
-        const cmd = b.addSystemCommand(&[_][]const u8{"strip", "-s"});
-        cmd.addArtifactArg(exe);
-        b.getInstallStep().dependOn(&cmd.step);
+        const strip_cmd = b.addSystemCommand(&[_][]const u8{"strip", "-s"});
+        strip_cmd.addArtifactArg(exe);
+        b.getInstallStep().dependOn(&strip_cmd.step);
+    }
+
+    // `size` option
+    const size = b.option(bool, "size", "Show the installed sizes") orelse false;
+    if (size) {
+        const exe_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ b.install_prefix, "/bin/zt" },
+        ) catch unreachable;
+        const desktop_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ b.install_prefix, "/", desktop_path},
+        ) catch unreachable;
+        const icon_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ b.install_prefix, "/", icon_path},
+        ) catch unreachable;
+        const size_cmd = b.addSystemCommand(
+            &[_][]const u8{
+                "du",
+                "-hc",
+                exe_absolute_path,
+                desktop_absolute_path,
+                icon_absolute_path,
+            }
+        );
+        b.getInstallStep().dependOn(&size_cmd.step);
     }
 
     const run_cmd = exe.run();
