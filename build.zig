4462555fa8eb472d8ffb803130c1c08d0e7f0a01
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-01-21 10:19:02 -0500

    Added archive functions to build.zig

--- build.zig
+++ build.zig
@@ -44,8 +44,8 @@ pub fn build(b: *Builder) void {
 
     // `strip` option
     const strip = b.option(bool, "strip", "Strip the installed executable") orelse false;
+    const strip_cmd = b.addSystemCommand(&[_][]const u8{"strip", "-s"});
     if (strip) {
-        const strip_cmd = b.addSystemCommand(&[_][]const u8{"strip", "-s"});
         strip_cmd.addArtifactArg(exe);
         b.getInstallStep().dependOn(&strip_cmd.step);
     }
@@ -75,6 +75,70 @@ pub fn build(b: *Builder) void {
             }
         );
         b.getInstallStep().dependOn(&size_cmd.step);
+    }
+
+    const archive = b.option(
+        bool,
+        "archive",
+        "Create an archive of the installed files",
+    ) orelse false;
+
+    const archive_fmt = b.option(
+        []const u8,
+        "archive_fmt",
+        "The compression format to use for the package archive. One of gz, bz2 or xz",
+    ) orelse "gz";
+
+    if (archive) {
+        const Format = enum { gz, bz2, xz };
+        const len = std.mem.len(archive_fmt);
+        const format = std.meta.stringToEnum(Format, archive_fmt[0..len]);
+        const prefix_basename = fs.path.basenamePosix(b.install_prefix);
+        const exe_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ prefix_basename, "/bin/zt" },
+        ) catch unreachable;
+        const desktop_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ prefix_basename, desktop_path},
+        ) catch unreachable;
+        const icon_absolute_path = fs.path.join(
+            b.allocator,
+            &[_][]const u8{ prefix_basename, icon_path},
+        ) catch unreachable;
+        if (format) |f| {
+            const tar_opts: []const u8 = switch (f) {
+                .gz => "-czf",
+                .bz2 => "-cjf",
+                .xz => "-cJf",
+            };
+            const ext = switch(f) {
+                .gz => "tar.gz",
+                .bz2 => "tar.bz2",
+                .xz => "tar.xz",
+            };
+            const archive_name = std.mem.join(
+                b.allocator,
+                ".",
+                &[_][]const u8{ prefix_basename, ext },
+            ) catch unreachable;
+            const tar_cmd = b.addSystemCommand(
+                &[_][]const u8{
+                    "tar",
+                    "--numeric-owner",
+                    "--owner=0",
+                    tar_opts,
+                    archive_name,
+                    exe_absolute_path,
+                    desktop_absolute_path,
+                    icon_absolute_path,
+                }
+            );
+            if (strip) {
+                tar_cmd.step.dependOn(&strip_cmd.step);
+            }
+            b.getInstallStep().dependOn(&tar_cmd.step);
+        }
     }
 
     const run_cmd = exe.run();
